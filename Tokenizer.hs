d3c869520c3041f506655c5a2cf55b447c10fad8
sam041 <sam041@bucknell.edu>
2025-11-26 23:40:19 -0500

    Created tokenize function, and fixed Token ADT constructors

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -1,20 +1,55 @@
+import Data.Char (isDigit, isAlpha, isAlphaNum, isSpace)
+
 data Token = 
-    If String |
-    Else String |
-    While String |
-    Variable String |
-    EqualsEquals String |
-    LessThan String |
-    Not String |
-    And String |
-    Or String |
-    LeftParenthesis String |
-    RightParenthesis String |
-    LeftBrace String |
-    RightBrace String |
-    Semicolon String |
-    AdditionOp String |
-    SubtractionOp String |
-    MultiplicationOp String |
-    DivisionOp String |
-    Number Int
+    If | -- "if"
+    Else | -- "else"
+    While | -- "while"
+    Variable String | -- e.g., "x", "y", "firstName", "lastName123"
+    Equals |
+    EqualsEquals |
+    LessThan | -- '<'
+    Not | -- "not"
+    And | -- "and"
+    Or | -- "or"
+    LeftParenthesis | -- '('
+    RightParenthesis | -- ')'
+    LeftBrace | -- '{'
+    RightBrace | -- '}'
+    Semicolon | -- ';'
+    AdditionOp | -- '+'
+    SubtractionOp | -- '-'
+    MultiplicationOp | -- '*'
+    DivisionOp | -- '/'
+    Number Int -- e.g., 123, 5865, 9023 
+
+tokenize :: String -> [Token]
+tokenize ('+' : restOfString) = AdditionOp : tokenize restOfString
+tokenize ('-' : restOfString) = SubtractionOp : tokenize restOfString
+tokenize ('*' : restOfString) = MultiplicationOp : tokenize restOfString
+tokenize ('/' : restOfString) = DivisionOp : tokenize restOfString
+tokenize ('<' : restOfString) = LessThan : tokenize restOfString
+tokenize ('(' : restOfString) = LeftParenthesis : tokenize restOfString
+tokenize (')' : restOfString) = RightParenthesis : tokenize restOfString
+tokenize ('{' : restOfString) = LeftBrace : tokenize restOfString
+tokenize ('}' : restOfString) = RightBrace : tokenize restOfString
+tokenize (';' : restOfString) = Semicolon : tokenize restOfString
+tokenize ('=' : '=' : restOfString) = EqualsEquals : tokenize restOfString
+tokenize ('=' : restOfString) = Equals : tokenize restOfString
+
+tokenize (firstCharacter : restOfString)
+    | isDigit firstCharacter = Number (read digits) : tokenize restOfInput
+    | isAlpha firstCharacter = checkKeyword wordString : tokenize afterWord
+    | isSpace firstCharacter = tokenize (dropWhile isSpace restOfString)
+
+    where
+        (digits, restOfInput) = span isDigit (firstCharacter : restOfString)
+        (wordString, afterWord) = span isAlphaNum (firstCharacter : restOfString)
+        checkKeyword :: String -> Token
+        checkKeyword s
+            | s == "if" = If
+            | s == "else" = Else
+            | s == "while" = While
+            | s == "not" = Not
+            | s == "and" = And
+            | s == "or" = Or
+            | otherwise = Variable s
